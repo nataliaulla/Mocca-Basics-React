d2eeba6a552291637fda3286b8310d0ab43afcb6
nataliaulla <natalia.ulla5@gmail.com>
2021-09-13 15:52:50 -0300

    Tercer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavBar from "./components/NavBar";
+const App = () => {
+    return(
+        <header>
+            <p>MOCCA BASICS</p>
+            <NavBar/>
+        </header>
+    )
+}
+
+
+
+export default App
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <a href="#">Nuevos Ingresos</a>
+            <a href="#">Todo Mocca</a>
+            <a href="#">Sale!</a>
+            <a href="#">Envíos y Medios de pago</a>
+            <a href="#">Contactanos!</a>
+        </nav>
+     )
+}
+
+export default NavBar
