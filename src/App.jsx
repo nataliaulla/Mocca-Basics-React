3d822f4d6854927ddfa941f43624a12b4dfa6a72
nataliaulla <natalia.ulla5@gmail.com>
2021-09-15 19:11:25 -0300

    CartWidget y ItemListContainer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import NavBar from "./components/NavBar";
 import "./components/estilos.css"
+import CartWidget from "./components/CartWidget";
+import ItemListContainer from "./components/ItemListContainer";
+
+
 const App = () => {
     return(
+        
+        <>
         <header>
             <p>MOCCA BASICS</p>
-            <NavBar/>
+            <CartWidget />
+            <NavBar />
+            <ItemListContainer titulo="Bienvenido a Mocca Basics!" />
         </header>
+        <div>
+        <ItemListContainer titulo="Bienvenido a Mocca Basics!" />
+        </div></>
+        
+    
     )
 }
 
---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,10 @@
+
+const ItemListContainer = ({titulo}) => {
+    return (
+        <div>
+            <h1 className="titulo">{titulo}</h1>
+        </div>
+     )
+}
+
+export default ItemListContainer
