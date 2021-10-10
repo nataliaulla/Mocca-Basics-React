dc2ea04de0e289137516f5687e2bb8d5e4040bda
nataliaulla <natalia.ulla5@gmail.com>
2021-10-10 18:45:05 -0300

    Desafío Cart View

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { useContext } from "react";
 import CartContext from "../context/CartContext";
+import { Link } from "react-router-dom"
 
 
 const Cart = () => {
     const {carrito} = useContext(CartContext);
     const listaDeTotales = [];
+
+if (carrito.length === 0){
+    return (
+        <div>
+            <p>No hay productos en el carrito!</p>
+            <Link to= "/">
+            <a className="btn btn-light mt-3" >Volver al Home</a>
+            </Link>
+        </div>
+    )
+}else{
 
     return (
         <>
@@ -15,8 +27,9 @@ const Cart = () => {
                 const total = producto.precio * producto.cantidad;
                 listaDeTotales.push(total);
                 return (
-                <li>{producto.titulo} 
-                Cantidad: {producto.cantidad}Precio: ${producto.precio}{""} Total: ${total}
+                <li>
+                Producto: {producto.titulo}. 
+                 Cantidad: {producto.cantidad} Precio: ${producto.precio}{""} Total: ${total}
                 </li>
                 );
             
@@ -26,6 +39,7 @@ const Cart = () => {
         <h2>Total compra: ${listaDeTotales.reduce((prev, next) => prev + next)}</h2>
         </>
     )
+}
 }
 
 
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,10 +1,20 @@
 import "./estilos.css"
 import { Link } from "react-router-dom"
+import Badge from "react-bootstrap/Badge"
+import { useContext } from 'react'
+import CartContext from "../context/CartContext"
+
+
 
 const CartWidget = () => {
+    
+
+    
     return (
         <Link to="/cart">
         <span className="material-icons">shopping_cart</span>
+         <Badge bg="light"></Badge>
+  
         </Link>
         
      )
